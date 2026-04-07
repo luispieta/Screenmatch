ba8b52f8364d3ba4f19d341f17e1ce59dfab3032
luispieta <lufelipepieta@gmail.com>
2026-04-07 07:46:49 -0300

    Feat: Convertendo dados para depois obter os dados que queremos

--- src/main/java/br/com/screenmatch/domain/Series/DadosSerie.java
+++ src/main/java/br/com/screenmatch/domain/Series/DadosSerie.java
@@ -1,7 +1,9 @@
 package br.com.screenmatch.domain.Series;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record DadosSerie(
                         @JsonAlias("Title") String titulo,
                         @JsonAlias("totalSeasons") Integer totalTemporadas,
