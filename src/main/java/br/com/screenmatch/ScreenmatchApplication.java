587404f94ce4970594979b4f523b1a9256bb555c
luispieta <lufelipepieta@gmail.com>
2026-04-14 07:41:41 -0300

    Feat: Modelando episódios

--- src/main/java/br/com/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/screenmatch/ScreenmatchApplication.java
@@ -2,7 +2,9 @@ package br.com.screenmatch;
 
 import br.com.screenmatch.Service.ConsumoApi;
 import br.com.screenmatch.Service.ConverteDados;
+import br.com.screenmatch.domain.Series.DadosEpisodio;
 import br.com.screenmatch.domain.Series.DadosSerie;
+import br.com.screenmatch.domain.Series.DadosTemporada;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.CommandLineRunner;
@@ -19,8 +21,16 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		var consumoApi = new ConsumoApi();
 		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=Rick+and+Morty&apiKey=6585022c");
 		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
+		DadosSerie dadosSerie = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dadosSerie);
+
+		json = consumoApi.obterDados("https://www.omdbapi.com/?t=Rick+and+Morty&Season=1&apiKey=6585022c");
+		DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+		System.out.println(dadosTemporada);
+
+		json = consumoApi.obterDados("https://www.omdbapi.com/?t=Rick+and+Morty&Season=1&Episode=2&apiKey=6585022c");
+		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
+		System.out.println(dadosEpisodio);
 
 	}
 
